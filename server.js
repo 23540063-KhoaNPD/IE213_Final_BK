a369ba806a9b27bb28f6efd0244d194abd77d68a
DangKhoa <dangkhoa06634@gmail.com>
2026-02-25 20:40:20 +0700

    fix cors

--- server.js
+++ server.js
@@ -17,22 +17,24 @@ const allowedOrigins = [
   `${process.env.FRONTEND_URL}`
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // cho Postman
-      if (allowedOrigins.indexOf(origin) === -1) {
-        return callback(new Error("Not allowed by CORS"), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  })
-);
+app.use(cors({ origin: "*" }));
 
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin) return callback(null, true); // cho Postman
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         return callback(new Error("Not allowed by CORS"), false);
+//       }
+//       return callback(null, true);
+//     },
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+//   })
+// );
 
+// app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
