886b02f5a1fa5e2ec0896a9c79ec500d229d7f7e
DangKhoa <dangkhoa06634@gmail.com>
2026-02-25 21:41:18 +0700

    update node mailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,11 +4,17 @@ import nodemailer from "nodemailer";
 // console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // ❗ 587 thì phải false
+  family: 4,     // ❗ ép dùng IPv4 (fix ENETUNREACH)
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 export const sendResetEmail = async (to, token) => {
